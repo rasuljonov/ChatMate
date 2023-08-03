1c7d878e17cba8eb31c49bc665a17d04d7442e2b
rasuljonov <hrasuljonob@webster.edu>
2023-08-03 08:45:21 +0500

    erorrMessageAdded

--- ChatMate/Controller/WelcomeViewController.swift
+++ ChatMate/Controller/WelcomeViewController.swift
@@ -17,6 +17,7 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         titleLabel.text = K.appName
         
       
